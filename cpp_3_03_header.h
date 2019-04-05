1797592ffae3b843301bc41e7bd5c9a4c5742592
Nikita Edgar <nikita-edgar.sitiajev@stud.vgtu.lt>
2019-04-05 19:41:14 +0200

    Primary code separation

---
+++ cpp_3_03_header.h
@@ -0,0 +1,31 @@
+#ifndef CPP_3_03_HEADER_H_INCLUDED
+#define CPP_3_03_HEADER_H_INCLUDED
+
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <numeric>
+#include <iomanip>
+#include <stdio.h>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+struct results
+{
+    string surname;
+    string name;
+    vector <float> hwm;
+    float examm;
+    float average;
+    float fpointsa;
+    float fpointsm;
+};
+
+void random(struct results *results);
+void mean(struct results *results);
+void median(struct results *results);
+void output(vector <results> students);
+
+#endif // CPP_3_03_HEADER_H_INCLUDED
